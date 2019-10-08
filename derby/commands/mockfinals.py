b96a6c64d60b69ad8c66cb5ee8e74c3d8c9d22e3
David Feeley <davidpfeeley@gmail.com>
2019-10-08 10:25:16 -0700

    mock finals command

---
+++ derby/commands/mockfinals.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+import random
+
+from django.conf import settings
+from django.utils import timezone
+
+from derby.core.models import Classes, RaceChart, Rounds
+
+
+class Command:
+    def __init__(self, args):
+        self.args = args
+        self.config = settings.ROUND_CONFIG['fastest']
+        self.parent_class = Classes.objects.get(pk=self.config['class_id'])
+        self.round = Rounds.objects.get(pk=self.config['round_id'])
+        self.min = 2.8
+        self.max = 3.4
+        self.range = self.max - self.min
+
+    def run(self):
+        results = list(RaceChart.objects.filter(classid=self.parent_class, round=self.round))
+        byheat = defaultdict(list)
+        for result in results:
+            result.finishtime = self.random_time
+            result.completed = timezone.now()
+            result.save()
+            byheat[result.heat].append(result)
+        for heat in byheat.values():
+            heat.sort(key=lambda i: i.finishtime)
+            for i, result in enumerate(heat, 1):
+                result.points = result.finishplace = i
+                result.save()
+
+    @property
+    def random_time(self):
+        return self.min + random.random() * self.range
