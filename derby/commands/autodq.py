ceed00e77e255223fa13b4f11d04ad7bed6962ec
David Feeley <davidpfeeley@gmail.com>
2022-01-22 21:38:14 -0800

    When autodq'ing, only do so when a racechart entry has actually been raced (ie. has a finishtime)

--- derby/commands/autodq.py
+++ derby/commands/autodq.py
@@ -12,7 +12,7 @@ class Command():
         self.args = args
 
     def run(self):
-        entries_to_dq = RaceChart.objects.filter(racer__carnumber__in=settings.AUTO_DQ).select_related('racer')
+        entries_to_dq = RaceChart.objects.filter(racer__carnumber__in=settings.AUTO_DQ).exclude(finishtime__isnull=True).select_related('racer')
         for entry in entries_to_dq:
             logger.info(f'About to DQ {entry.racer}: {entry.resultid=} {entry.finishtime=} {entry.finishplace=}')
             entry.finishtime = settings.DNF_THRESHOLD + 1.0
